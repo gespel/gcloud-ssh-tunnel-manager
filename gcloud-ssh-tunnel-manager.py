d69bbf845d3e7d254319bae783a768bb600583b2
gespel <heimbrodt@uni-potsdam.de>
2025-03-17 09:19:12 +0100

    added yaml and removed ui

--- gcloud-ssh-tunnel-manager.py
+++ gcloud-ssh-tunnel-manager.py
@@ -1,4 +1,6 @@
 import subprocess
+import yaml
+import pprint
 import tkinter as tk
 from functools import partial
 from tkinter import ttk
@@ -6,10 +8,10 @@ from threading import Thread
 
 
 def tunnel(name, zone, local_port, remote_port):
-    print(
-        subprocess.run(["gcloud", "compute", "ssh", name, "--zone", zone, "--", "-N", "-L", f"{local_port}:localhost:{remote_port}"],
-                       stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'))
-
+    exec_string = f"gcloud compute ssh {name} --zone {zone} -- -N -L {local_port}:localhost:{remote_port}"
+    #print(exec_string)
+    #print(subprocess.run(["gcloud", "compute", "ssh", name, "--zone", zone, "--", "-N", "-L", f"{local_port}:localhost:{remote_port}"], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'))
+    print(subprocess.run([exec_string], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'))
 class GCloudSSHTunnelManager:
     def __init__(self, name, zone):
         self.zone = zone
@@ -19,47 +21,15 @@ class GCloudSSHTunnelManager:
         t = Thread(target=tunnel, args=(self.name, self.zone, local_port, remote_port))
         t.start()
 
-
-def new_tunnel_dialog(name, zone, local_port, remote_port):
-    label = tk.Label(root, text=f"{name} in {zone} -> {local_port}:localhost:{remote_port}", bg="gray40", fg="white")
-    label.grid(row=2 + len(labels), column=0, padx=10, pady=5)
-    labels.append(label)
-    gcm = GCloudSSHTunnelManager(name, zone)
-    gcm.start_tunnel(local_port, remote_port)
-
 if __name__ == '__main__':
-    root = tk.Tk()
-    name_var = tk.StringVar()
-    zone_var = tk.StringVar()
-    local_port_var = tk.StringVar()
-    remote_port_var = tk.StringVar()
-    root.resizable(False, False)
-    root.minsize(400, 200)
-    root.title("Tunnel Manager")
-    root.config(bg="gray15")
-
-    labels = []  # Liste zum Speichern der Labels
-
-    tk.Label(root, text="Current Tunnels:", bg="gray15", fg="white").grid(row=0, column=0, padx=10, pady=5)
-
-    separator = ttk.Separator(root, orient="vertical")
-    separator.grid(row=0, column=1, rowspan=100, sticky="ns", padx=5)
-
-    tk.Label(root, text="Instance name:", bg="gray15", fg="white").grid(row=0, column=2, padx=10, pady=5)
-    tk.Entry(root, textvariable=name_var, bg="gray15", fg="white").grid(row=0, column=3, padx=10, pady=5)
-
-    tk.Label(root, text="Zone:", bg="gray15", fg="white").grid(row=1, column=2, padx=10, pady=5)
-    tk.Entry(root, textvariable=zone_var, bg="gray15", fg="white").grid(row=1, column=3, padx=10, pady=5)
-
-    tk.Label(root, text="Local port:", bg="gray15", fg="white").grid(row=2, column=2, padx=10, pady=5)
-    tk.Entry(root, textvariable=local_port_var, bg="gray15", fg="white").grid(row=2, column=3, padx=10, pady=5)
-
-    tk.Label(root, text="Remote port:", bg="gray15", fg="white").grid(row=3, column=2, padx=10, pady=5)
-    tk.Entry(root, textvariable=remote_port_var, bg="gray15", fg="white").grid(row=3, column=3, padx=10, pady=5)
-
-    tk.Button(root, text="Start New Tunnel", command=lambda: new_tunnel_dialog(
-        name_var.get(), zone_var.get(), local_port_var.get(), remote_port_var.get()
-    )).grid(row=4, column=2, padx=10, pady=5)
-
-    root.mainloop()
-
+    with open("backends.yaml", "r") as f:
+        config = yaml.safe_load(f)
+        for server in config["backends"]:
+            name = server["name"]
+            zone = server["zone"]
+            ports = server["ports"]
+            gcm = GCloudSSHTunnelManager(name, zone)
+            for port in ports:
+                local_port = port["local_port"]
+                remote_port = port["remote_port"]
+                gcm.start_tunnel(local_port, remote_port)
